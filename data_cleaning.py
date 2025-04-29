690479e0b17906bf4cb8de8ad7bd6a6ca72a4f62
Alex Dioso <adioso@uw.edu>
2025-04-29 12:48:16 +0000

    Remove temperatures less then -40.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,7 @@
 def clean_data(data):
     if "temperature" in data.columns:
         data.loc[data["temperature"] >= 9999, "temperature"] = None
+        data.loc[data["temperature"] < -40, "temperature"] = None
     if "salinity" in data.columns:
         data.loc[data["salinity"] >= 9999, "salinity"] = None
     if "significant_height" in data.columns:
