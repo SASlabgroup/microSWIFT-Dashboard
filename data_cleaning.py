59af0e501ed4235effde151b83a405e0abf459c9
Alex Dioso <adioso@uw.edu>
2025-04-29 13:05:25 +0000

    Fix filtering temperature.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,8 @@
 def clean_data(data):
     if "temperature" in data.columns:
-        data.loc[data["temperature"] >= 9999, "temperature"] = None
-        data.loc[data["temperature"] < -40, "temperature"] = None
+        data.loc[
+            (data["temperature"] >= 9999) | (data["temperature"] < -40), "temperature"
+        ] = None
     if "salinity" in data.columns:
         data.loc[data["salinity"] >= 9999, "salinity"] = None
     if "significant_height" in data.columns:
